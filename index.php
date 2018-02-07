bbfe9eba33b1800f174a80b74650dba774f9849b
unknown <ashfabio@gmail.com>
2018-02-07 15:45:25 -0200

    Métodos de lista

--- index.php
+++ index.php
@@ -8,10 +8,25 @@ $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios);*/
 
-$root = new Usuario();
+//CARREGA 1 USUÁRIO
+//$root = new Usuario();
+//$root->loadById(1);
+//echo $root;
 
-$root->loadById(1);
 
-echo $root;
+//CARREGA UMA LISTA DE USUÁRIOS
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+
+//CARREGA UMA LISTA DE USUÁRIOS BUSCANDO PELO LOGIN
+//$search = Usuario::search("jo");
+//echo json_encode($search);
+
+//CARREGA UM USUÁRIO USANDO O LOGIN E A SENHA
+$usuario = new Usuario();
+$usuario->login("jose", "1234567890");
+
+echo $usuario;
 
 ?>
